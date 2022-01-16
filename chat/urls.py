907e52db21d060acfbb84e46ffd8f3f179d10c5f
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2022-01-16 23:09:11 +0100

    working on html

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,7 @@
 """
 from django.urls import path
 
+from chat.views.add_view import AddView
 from chat.views.detail_view import DetailView
 from chat.views.overview_view import OverviewView
 
@@ -10,6 +11,7 @@ from chat.views.overview_view import OverviewView
 app_name = 'chat'
 
 urlpatterns = [
+    path('add/', AddView.as_view(), name='add'),
     path('detail/', DetailView.as_view(), name='detail'),
     path('overview/', OverviewView.as_view(), name='overview')
 ]
