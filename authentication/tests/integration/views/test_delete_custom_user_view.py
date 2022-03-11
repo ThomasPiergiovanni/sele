5058171ea1ee54cfd9250a0a466dae6a0ba73517
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2022-03-11 06:39:28 +0100

    DeleteCustomUserView in authentication.views completed. Unit and integration tests OK

--- authentication/tests/integration/views/test_delete_custom_user_view.py
+++ authentication/tests/integration/views/test_delete_custom_user_view.py
@@ -4,6 +4,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from authentication.models import CustomUser
 from authentication.tests.emulation.authentication_emulation import (
     AuthenticationEmulation
 )
@@ -15,28 +16,15 @@ class DeleteCustomViewTest(TestCase):
     def setUp(self):
         self.authentication_emulation = AuthenticationEmulation()
 
-    def test_get_with_view_template(self):
+    def test_get_with_authenticated(self):
         self.authentication_emulation.emulate_custom_user()
         self.client.login(email='user1@email.com', password='xxx_Xxxx')
         response = self.client.get(
             '/authentication/delete_custom_user/', follow=True
         )
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed('authentication/delete_custom_user.html')
-        for message in response.context['messages']:  
-            self.assertEqual(message.message, "Authentification requise")
-            self.assertEqual(message.level_tag, "error")
-        # self.assertEqual(self
-        #     response.context['messages']._loaded_data[0].message, 
-        #     "Compte utilisateur supprimé"
-        # )
-        # self.assertEqual(
-        #     response.context['messages']._loaded_data[0].level_tag, 
-        #     "success"
-        # )
 
-
-    def test_get_with_unauthentified_user(self):
+    def test_get_with_unauthenticated_user(self):
         response = self.client.get(
             '/authentication/delete_custom_user/', follow=True
         )
@@ -44,6 +32,55 @@ class DeleteCustomViewTest(TestCase):
             response.redirect_chain[0][0],reverse('information:home')
         )
         self.assertEqual(self.client.session.get('_auth_user_id'), None)
+        self.assertEqual(
+            response.context['messages']._loaded_data[0].message, 
+            "Authentification requise"
+        )
+        self.assertEqual(
+            response.context['messages']._loaded_data[0].level_tag, 
+            "error"
+        )
+    
+    def test_post_with_authenticated_user(self):
+        self.authentication_emulation.emulate_custom_user()
+        self.client.login(email='user1@email.com', password='xxx_Xxxx')
+        response = self.client.post(
+            '/authentication/delete_custom_user/',
+            follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.redirect_chain[0][0], reverse('information:home')
+        )
+        try:
+            custom_user = CustomUser.objects.get(email__exact='user1@email.com')
+        except:
+            custom_user = False
+        self.assertFalse(custom_user)
+        self.assertEqual(
+            response.context['messages']._loaded_data[0].message, 
+            "Suppression de compte réussie"
+        )
+        self.assertEqual(
+            response.context['messages']._loaded_data[0].level_tag, 
+            "success"
+        )
+
+    def test_post_with_unauthenticated_user(self):
+        self.authentication_emulation.emulate_custom_user()
+        response = self.client.post(
+            '/authentication/delete_custom_user/',
+            follow=True
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.redirect_chain[0][0], reverse('information:home')
+        )
+        try:
+            custom_user = CustomUser.objects.get(email__exact='user1@email.com')
+        except:
+            custom_user = False
+        self.assertTrue(custom_user)
         self.assertEqual(
             response.context['messages']._loaded_data[0].message, 
             "Authentification requise"
--- authentication/views/delete_custom_user_view.py
+++ authentication/views/delete_custom_user_view.py
@@ -1,6 +1,7 @@
 
 """Delete custom user view module.
 """
+from django.contrib.auth import logout
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.views import View
@@ -33,43 +34,26 @@ class DeleteCustomUserView(View):
                 )
             return redirect(self.alternative_view_name)
 
-    # def post(self, request):
-    #     """Edit custom user page view method on client post request. Create
-    #     CustomUser into the DB. After Voting creation, user is redirect to
-    #      voting overview page.
-    #     """
-    #     if request.user.is_authenticated:
-    #         form = EditCustomUserForm(request.POST)
-    #         if form.is_valid():
-    #             collectivity = Manager().check_collectivity(
-    #                 form.cleaned_data['postal_code'],
-    #                 form.cleaned_data['collectivity']
-    #             )
-    #             if collectivity:
-    #                 Manager().edit_custom_user(request, form, collectivity)
-    #                 Manager().activate_collectivity(collectivity)
-    #                 messages.add_message(
-    #                     request,
-    #                     messages.SUCCESS,
-    #                     "Mise à jours réussie",
-    #                 )
-    #                 return redirect(self.alternative_view_name)
-    #             else:
-    #                 messages.add_message(
-    #                     request,
-    #                     messages.ERROR,
-    #                     "Le couple \"code postal\" et \"ville\" n'est pas valide.",
-    #                 )
-    #                 return render(request, self.view_template, {'form': form})
-    #         else:
-    #             return render(
-    #                 request, self.view_template, {'form': form}
-    #             )
-    #     else:
-    #         messages.add_message(
-    #                 request,
-    #                 messages.ERROR,
-    #                 "Authentification requise",
-    #             )
-    #         return redirect(self.alternative_view_name)
+    def post(self, request):
+        """Delete custom user view method on client post request. Delete
+        CustomUser into the DB. After deletion, user is redirect to
+        home page.
+        """
+        if request.user.is_authenticated:
+            custom_user = CustomUser.objects.get(pk=request.user.id)
+            custom_user.delete()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Suppression de compte réussie",
+            )
+            logout(request)
+            return redirect(self.alternative_view_name)
+        else:
+            messages.add_message(
+                    request,
+                    messages.ERROR,
+                    "Authentification requise",
+                )
+            return redirect(self.alternative_view_name)
 
