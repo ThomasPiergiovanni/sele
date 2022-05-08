3f55d7e5efad1ddba7bf78174ae0cfb6bc72eeef
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2022-05-08 14:19:40 +0200

    Test vote use case in vote.tests.fubctionnal completed. Functionnal test OK

---
+++ vote/tests/functional/test_vote_use_case.py
@@ -0,0 +1,133 @@
+"""Test rating use case test module. Functional test
+"""
+import os
+
+from time import sleep
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+
+from authentication.tests.emulation.authentication_emulation import (
+    AuthenticationEmulation
+)
+from vote.tests.emulation.vote_emulation import VoteEmulation
+
+
+class CreateVotingUseCaseTest(StaticLiveServerTestCase):
+    """Test create voting use case test class
+    """
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        firefox_options = webdriver.FirefoxOptions()
+        if os.name == 'nt':
+            firefox_options.headless = False
+            cls.browser = webdriver.Firefox(
+                executable_path=str(
+                    r'D:\02_oc\13_p13\config\settings\geckodriver.exe'
+                ),
+                options=firefox_options,
+            )
+        if os.name == 'posix':
+            firefox_options.headless = True
+            cls.browser = webdriver.Firefox(
+                executable_path=str('/usr/local/bin/geckodriver'),
+                options=firefox_options,
+            )
+        cls.browser.implicitly_wait(30)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
+
+    def setUp(self):
+        # The user logs to the login page
+        self.auth_emulation = AuthenticationEmulation()
+        self.auth_emulation.emulate_custom_user()
+        self.vote_emulation = VoteEmulation()
+        self.vote_emulation.emulate_voting_method()
+        self.vote_emulation.emulate_voting()
+        self.browser.get(
+            '%s%s' % (
+                self.live_server_url,
+                '/authentication/login/'
+            )
+        )
+
+    def test_vote_use_case(self):
+        # The user types its email and password.
+        sleep(2)
+        self.browser.find_element_by_id('input_login_email')\
+        .send_keys('user1@email.com')
+        sleep(1)
+        self.browser.find_element_by_id('input_login_password')\
+        .send_keys('xxx_Xxxx')
+        sleep(1)
+
+        # The user clicks then "Se connecter" button and lands on the
+        # home page.
+        self.browser.find_element_by_id('login_button').click()
+        self.assertIn(
+            'sel-e',
+            self.browser.find_element_by_tag_name('h1').text
+        )
+        sleep(1)
+
+        self.browser.get(
+            '%s%s' % (
+                self.live_server_url,
+                '/vote/create_vote/1/'
+            )
+        )
+
+        # The user selects "Mon groupe Local" on the left navigation sidebar
+        # and the selects "Votes"
+        # self.browser.find_element_by_id('sidebar_my_local_group').click()
+        # self.browser.find_element_by_id('sidebar_mlg_votings').click()
+        sleep(2)
+        self.assertIn(
+            self.browser.find_element_by_class_name('login-box-msg').text,
+            'Participer au vote',
+        )
+        self.browser.find_element_by_id('yes_button').click()
+        sleep(2)
+        self.assertIn(
+            self.browser.find_element_by_id('voting_result').text,
+            '100,0 %',
+        )
+        sleep(2)
+
+        #The user selects the "Créer une votation" button
+        # self.browser.find_element_by_id('go_to_create_voting_button').click()
+        # self.assertIn(
+        #     self.browser.find_element_by_tag_name('h1').text,
+        #     'Votation - Créer',
+        # )
+        # sleep(2)
+        
+        # The user fills the form anc select the Créer button. The user
+        # should land on the votings page and see is created voting
+        # on th top of the list.
+
+        # self.browser.find_element_by_id('input_voting_question')\
+        # .send_keys('Voulez-vous améliorer sel-e?')
+        # sleep(2)
+        # self.browser.find_element_by_id('input_voting_description')\
+        # .send_keys('bla bla bla')
+        # sleep(2)
+        # self.browser.find_element_by_id('input_voting_voting_method')\
+        # .send_keys('Majoritaire')
+        # sleep(2)
+        # self.browser.find_element_by_id('input_voting_opening_date')\
+        # .send_keys('2022-02-04')
+        # sleep(2)
+        # self.browser.find_element_by_id('input_voting_closure_date')\
+        # .send_keys('2022-02-11')
+        # sleep(2)
+        # self.browser.find_element_by_id('create_voting_button').click()
+        # self.assertIn(
+        #     self.browser.find_element_by_tag_name('td').text,'1'
+        # )
+        # sleep(2)
