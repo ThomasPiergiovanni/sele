19de414e75b7a724bda8127e66cfa0325f0d8c22
Thomas Piergiovanni <thomas.piergiovanni@outlook.fr>
2022-06-04 15:28:06 +0200

    Finalizing Readme and CGU template

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-ENV = 'test'
+ENV = 'staging'
 
 if ENV == 'test':
     from config.settings.testing_env import (
